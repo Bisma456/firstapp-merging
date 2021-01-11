9d2628525bc714d0617e689b417107d853791ad5
bisma456 <bismatouseef2020@gmail.com>
2021-01-11 13:09:47 +0500

    done

---
+++ backend/Users.js
@@ -0,0 +1,6 @@
+
+let users = [
+    {name:'Seema',id:1,email:'simi@gmail.com',password:'1111'},
+    {name:'fatima',id:2,email:'fatima@gmail.com',password:'2222'},
+    {name:'bisma',id:3,email:'bisma@gmail.com',password:'3333'},]
+module.exports = users
